6f1b7617dc03e3d69a5559b9e6867f7ac2523954
AlexAlbarran <alejandro.albarran.01@gmail.com>
2024-07-10 20:21:18 -0500

    First commit 1.3

--- SimpleBatch19Jenkins/src/Main.java
+++ SimpleBatch19Jenkins/src/Main.java
@@ -1,8 +1,10 @@
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
 
-        System.out.println("Hello and welcome!");
-
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Hello and welcome!");
+            Thread.sleep(2000);
+        }
     }
 }
